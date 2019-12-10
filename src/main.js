9cff5ea464d28cab81d84c6de47345cc83403bb5
BXK79 <bxkk79@163.com>
2019-12-10 17:55:46 +0800

    项目第一天打卡

--- src/main.js
+++ src/main.js
@@ -2,18 +2,35 @@ Vue.config.devtools = true
     //入口文件
 import Vue from 'vue'
 
-// 按需导入 Mint-UI组件
-import { Header } from 'mint-ui'
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+// 2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
+
+// 按需导入 Mint-UI组件    
+import { Header, Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 import app from './App.vue'
 
 // 导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
+
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
 
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app) // render 会把 el 指定的容器中，所有的内容都清空覆盖，所以 不要 把 路由的 router-view 和 router-link 直接写到 el 所控制的元素中
-
+    render: c => c(app), // render 会把 el 指定的容器中，所有的内容都清空覆盖，所以 不要 把 路由的 router-view 和 router-link 直接写到 el 所控制的元素中
+    router // 1.4 挂载路由对象到 VM 实例上
 })
