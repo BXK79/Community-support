0f6e1a9e610e8d7d60a0b297e0e32714ef032b2e
BXK79 <bxkk79@163.com>
2019-12-16 03:03:05 +0800

    项目第三天

--- src/main.js
+++ src/main.js
@@ -7,16 +7,27 @@ import VueRouter from 'vue-router'
 // 1.2 安装路由
 Vue.use(VueRouter)
 
+// 导入格式化时间的插件
+import moment from 'moment'
+// 定义全局的过滤器
+Vue.filter('dateFormat', function(dataStr, pattern = "YYYY-MM-DD HH:mm:ss") {
+    return moment(dataStr).format(pattern)
+})
+
 // 2.1 导入 vue-resource
 import VueResource from 'vue-resource'
 // 2.2 安装 vue-resource
 Vue.use(VueResource)
 
+// 设置请求的根路径
+Vue.http.options.root = 'http://www.liulongbin.top:3005';
+
 // 按需导入 Mint-UI组件    
-import { Header, Swipe, SwipeItem } from 'mint-ui'
+import { Header, Swipe, SwipeItem, Button } from 'mint-ui'
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Button.name, Button)
 
 import app from './App.vue'
 
